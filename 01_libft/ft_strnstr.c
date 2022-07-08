2c5c1c2c0724e30481bf25e1327955d21fed589c
Yongmin Park <yongmipa@c7r8s4.42seoul.kr>
2022-07-08 22:06:56 +0900

    오늘 작성한 코드

---
+++ 01_libft/ft_strnstr.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yongmipa <yongmipa@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 22:01:30 by yongmipa          #+#    #+#             */
+/*   Updated: 2022/07/08 22:05:52 by yongmipa         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	
+	if (!needle)
+		return (haystack);
+	
+}
